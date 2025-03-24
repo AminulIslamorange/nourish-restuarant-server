8ed19b9effaca453b8e99cc25931b4f094f80721
AminulIslamorange <aminulislamorange@gmail.com>
2025-03-24 21:21:43 +0600

    stripe setup

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -222,6 +223,25 @@ async function run() {
         admin = user?.role === 'admin';
       }
       res.send({ admin });
+
+    });
+
+
+
+    // payment gatway api
+
+    app.post('/create-payment-intent',async(req,res)=>{
+      const {price}=req.body;
+      const amount=parseInt(price*100);
+      const paymentIntent=await stripe.paymentIntents.create({
+        amount:amount,
+        currency:'usd',
+        payment_method_types:['card']
+      });
+      res.send({
+        clientSecret:paymentIntent.client_secret
+
+      })
 
     })
 
